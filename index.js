7eb5fa60c27c3bee99ed8fccd695cbba734d3ea7
Yogesh Kumar <yogesh.kumar2@sapient.com>
2020-07-25 12:26:24 +0530

    Brightcove player in ES5

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import React, { useRef, useEffect } from "react";
 import brightcovePlayerLoader from "@brightcove/player-loader";
 
-const ReactPlayerLoader = ({
-  wrapperProps,
-  wrapperClassName,
+const ReactPlayerLoader = function ({
+  wrapperProps = {},
+  wrapperClassName = "",
   accountId,
   onSuccess,
   onFailure,
@@ -14,38 +14,47 @@ const ReactPlayerLoader = ({
   playlistId,
   playlistVideoId,
   ...rest
-}) => {
+}) {
   const divRef = React.useRef();
 
-  useEffect(() => {
-    divRef.current.innerHTML = "";
-    const options = {
-      accountId,
-      videoId,
-      catalogSearch,
-      catalogSequence,
-      playlistId,
-      playlistVideoId,
-      refNode: divRef.current,
-      ...rest,
-    };
+  useEffect(
+    function () {
+      divRef.current.innerHTML = "";
+      const options = {
+        accountId,
+        videoId,
+        catalogSearch,
+        catalogSequence,
+        playlistId,
+        playlistVideoId,
+        refNode: divRef.current,
+        ...rest,
+      };
 
-    brightcovePlayerLoader(options)
-      .then(function (success) {
-        onSuccess(divRef, success);
-      })
-      .catch(function (error) {
-        onFailure(divRef, error);
-      });
-  }, [videoId, catalogSearch, catalogSequence, playlistId, playlistVideoId]);
-
-  return (
-    <div
-      className={`${wrapperClassName} brightcove-react-player-loader`}
-      {...wrapperProps}
-      ref={divRef}
-    />
+      brightcovePlayerLoader(options)
+        .then(function (success) {
+          onSuccess(divRef, success);
+        })
+        .catch(function (error) {
+          onFailure(divRef, error);
+        });
+    },
+    [videoId, catalogSearch, catalogSequence, playlistId, playlistVideoId]
   );
+
+  const props = Object.assign(
+    {
+      className: wrapperClassName
+        ? wrapperClassName + " brightcove-react-player-loader"
+        : "brightcove-react-player-loader",
+    },
+    wrapperProps,
+    {
+      ref: divRef,
+    }
+  );
+
+  return React.createElement("div", props);
 };
 
 export default ReactPlayerLoader;

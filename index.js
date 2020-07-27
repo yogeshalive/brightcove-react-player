269a7eaa662df95f0f8920ffb6753d9a7d6a8c25
Yogesh Kumar <yogesh.kumar2@sapient.com>
2020-07-27 15:26:31 +0530

    Enabled for IE11

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 const React = require("react");
 const brightcovePlayerLoader = require("@brightcove/player-loader").default;
 
-const ReactPlayerLoader = function ({
-  wrapperProps = {},
-  wrapperClassName = "",
-  accountId,
-  onSuccess,
-  onFailure,
-  baseUrl,
-  videoId,
-  catalogSearch,
-  catalogSequence,
-  playlistId,
-  playlistVideoId,
-  ...rest
-}) {
+const ReactPlayerLoader = function (params) {
+  const wrapperProps = params.wrapperProps || {};
+  const wrapperClassName = params.wrapperClassname || "";
+  const accountId = params.accountId;
+  const onSuccess = params.onSuccess;
+  const onFailure = params.onFailure;
+  const baseUrl = params.baseUrl;
+  const videoId = params.videoId;
+  const catalogSearch = params.catalogSearch;
+  const catalogSequence = params.catalogSequence;
+  const playlistId = params.playlistId;
+  const playlistVideoId = params.playlistVideoId;
+  const rest = Object.assign({}, params);
+
+  delete rest.wrapperProps;
+  delete rest.wrapperClassName;
+  delete rest.accountId;
+  delete rest.onSuccess;
+  delete rest.onFailure;
+  delete rest.baseUrl;
+  delete rest.videoId;
+  delete rest.catalogSearch;
+  delete rest.catalogSequence;
+  delete rest.playlistId;
+  delete rest.playlistVideoId;
+
   const divRef = React.useRef();
 
   React.useEffect(
     function () {
       divRef.current.innerHTML = "";
-      const options = {
-        accountId,
-        videoId,
-        catalogSearch,
-        catalogSequence,
-        playlistId,
-        playlistVideoId,
+      const options = Object.assign({}, rest, {
+        accountId: accountId,
+        videoId: videoId,
+        catalogSearch: catalogSearch,
+        catalogSequence: catalogSequence,
+        playlistId: playlistId,
+        playlistVideoId: playlistVideoId,
         refNode: divRef.current,
-        ...rest,
-      };
-
+      });
       brightcovePlayerLoader(options)
         .then(function (success) {
           onSuccess(success, divRef);
